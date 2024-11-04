ff8772723ed423938aca7a3760cac3dc7d900aa2
sumaiyaosd-cloud <samiyanabila112@gmail.com>
2024-11-04 21:21:12 +0600

    Create Sumaiya.java
    my commit

---
+++ Sumaiya.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String[] arg){
+        System.out.println("Sinister Cypher");
+        System.out.println("Your Dark Guardian of Digital Secrets");
+
+        new PasswordStorage();
+    }
+}
+class Main{
+    public static void main(String[] arg){
+        System.out.println("Sinister Cypher");
+        System.out.println("Your Dark Guardian of Digital Secrets");
+    }
+}
