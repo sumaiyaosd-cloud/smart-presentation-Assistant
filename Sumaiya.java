f9940641c38a1fe1a30e6ffd9382ed5d3351704f
sumaiyaosd-cloud <samiyanabila112@gmail.com>
2024-11-19 13:08:57 +0600

    removed a line

--- Sumaiya.java
+++ Sumaiya.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] arg){
         System.out.println("Smart Presentation Assistant");
-        System.out.println("Your personalized Ai helper");
+
     }
 }
