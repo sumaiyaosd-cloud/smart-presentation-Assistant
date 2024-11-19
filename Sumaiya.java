1d6185cfbe5f2dcbd01c5dafa2bdbc67a069e40d
sumaiyaosd-cloud <samiyanabila112@gmail.com>
2024-11-19 12:57:50 +0600

    removed redundant codes

--- Sumaiya.java
+++ Sumaiya.java
@@ -1,14 +1,6 @@
 class Main{
     public static void main(String[] arg){
-        System.out.println("Sinister Cypher");
-        System.out.println("Your Dark Guardian of Digital Secrets");
-
-        new PasswordStorage();
-    }
-}
-class Main{
-    public static void main(String[] arg){
-        System.out.println("Sinister Cypher");
-        System.out.println("Your Dark Guardian of Digital Secrets");
+        System.out.println("Smart Presentation Assistant");
+        System.out.println("Your personalized Ai helper");
     }
 }
